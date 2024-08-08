ebd3ad259f1b2335434ed350585187ff84c486e0
Abhishek Singh <167851013+S-Abhishekk@users.noreply.github.com>
2024-08-08 07:59:52 +0000

    onedown

---
+++ arithmetic.py
@@ -0,0 +1,16 @@
+#Abhishek Singh_500105942
+
+num1= int(input("Enter the first number: "))
+num2= int(input("Enter the second number: "))
+
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+mod = num1 % num2 
+
+print('Sum of ',num1 ,'and' ,num2 ,'is :',add)
+print('Difference of ',num1 ,'and' ,num2 ,'is :',sub)
+print('Product of' ,num1 ,'and' ,num2 ,'is :',mul)
+print('Division of ',num1 ,'and' ,num2 ,'is :',div)
+print('Modulus of ',num1 ,'and' ,num2 ,'is :',mod)
